2b78634bc338294d1f6c55c79d10c83bb93ca296
TaherAmahri <141448060+TaherAmahri@users.noreply.github.com>
2023-08-05 18:06:50 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculate from "./component/calculate";
+
+function App() {
+
+  return (  
+    <>
+    <Calculate/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/calculate.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+const Calculate =()=>{
+    const [result,setResult]=useState("");
+
+    const handleClick=(e)=>{
+      setResult(result.concat(e.target.id));
+    };
+  
+    const clear=()=>{
+      setResult("");
+    };
+  
+    const deleteElement=()=>{
+      setResult(result.slice(0,-1));
+    };
+  
+    const calculate=()=>{
+      try{
+        setResult(eval(result).toString());
+      }catch(error){
+        setResult("Invalid");
+      }
+    };
+    return(
+        <div className="container">
+        <input type="text" value={result} disabled />
+      <div>
+        <button className="operator" onClick={clear}>AC</button>
+        <button className="operator" onClick={deleteElement}>DE</button>
+        <button id="." className="operator" onClick={handleClick}>.</button>
+        <button id="/" className="operator" onClick={handleClick}>/</button>
+        </div>
+
+        <div>
+        <button id="7" className="number" onClick={handleClick}>7</button>
+        <button id="8" className="number" onClick={handleClick} >8</button>
+        <button id="9" className="number" onClick={handleClick}>9</button>
+        <button id="*" className="operator" onClick={handleClick}>*</button>
+        </div>
+
+        <div>
+        <button id="4" className="number" onClick={handleClick}>4</button>
+        <button id="5" className="number" onClick={handleClick}>5</button>
+        <button id="6" className="number" onClick={handleClick}>6</button>
+        <button id="-" className="operator" onClick={handleClick}>-</button>
+        </div>
+
+        <div>
+        <button id="1" className="number" onClick={handleClick}>1</button>
+        <button id="2" className="number" onClick={handleClick}>2</button>
+        <button id="3" className="number" onClick={handleClick}>3</button>
+        <button id="+" className="operator" onClick={handleClick}>+</button>
+        </div>
+
+        <button id="00" className="number" onClick={handleClick}>00</button>
+        <button id="0" className="number" onClick={handleClick}>0</button>
+        <button id="=" className="operator col-1" onClick={calculate}>=</button>
+      </div>
+    )
+}
+export default Calculate;
